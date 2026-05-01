d7d27eb9b4dcc4226bfddb513b5dd42109cacca5
Cdt226 <bricezoungrana@outlook.fr>
2026-05-01 00:44:50 +0000

    Email expéditeur splitmeapp.com

--- api/send-email.js
+++ api/send-email.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        from: 'SplitLy <onboarding@resend.dev>',
+        from: 'SplitLy <noreply@splitmeapp.com>',
         to,
         subject,
         html,
